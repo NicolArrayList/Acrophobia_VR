000f6b2bdac2fcbd28278665c7b23b686aeb9dd9
Ortech <aurelienchevallereau@icloud.com>
2022-04-23 15:04:42 -0500

    Ajout suivi du casque

--- Assets/ContinuousMovement.cs
+++ Assets/ContinuousMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Unity.XR.CoreUtils;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class ContinuousMovement : MonoBehaviour
@@ -9,6 +10,7 @@ public class ContinuousMovement : MonoBehaviour
     public float speed = 1;
     public XRNode inputSource;
 
+    private XROrigin rig;
     private Vector2 inputAxis;
     private CharacterController character;
 
@@ -16,6 +18,7 @@ public class ContinuousMovement : MonoBehaviour
     void Start()
     {
         character = GetComponent<CharacterController>();
+        rig = GetComponent<XROrigin>();
     }
 
     // Update is called once per frame
@@ -27,7 +30,8 @@ public class ContinuousMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y);
+        Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
 
         character.Move(direction * Time.fixedDeltaTime * speed);
     }
